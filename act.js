4aa0741c58bd925611f236bf2f06c69cff5dc8a1
Tobias Buschor <tobias.buschor@shwups.ch>
2021-07-12 13:11:47 +0200

    July 12, 2021 1:11 PM

--- act.js
+++ act.js
@@ -2,36 +2,54 @@
 
 
 
-Act = class extends EventTarget {
+export class Act extends EventTarget {
     constructor(target, parent){
-        this.target = target;
+        super();
+        this.$target = target;
         this.$parent = parent;
     }
-    $set(name, value) {
-        if (this.target[name]===value) return; //
+    get $proxy(){
+        return new Proxy(this, {
+            get(target, prop, receiver) {
+                return target.$get(prop);
+            },
+            set(target, prop, value, receiver) {
+                return target.$set(prop, value);
+            }
+        });
     }
-    $get(name) {
-        return this.target[name];
-        this.dispatchEvent(new CustomEvent('get', {name}));
-        this.$trigger('get-in');
+    $set(prop, value) {
+        console.log(this)
+        if (this.$target[prop]!==value) {
+            this.dispatchEvent(new CustomEvent('set', {prop, value}));
+            this.$target[prop] = value;
+        }
+        return true;
+    }
+    $get(prop) {
+        this.dispatchEvent(new CustomEvent('get', {prop}));
+        return this.$target[prop];
     }
 }
 
 
 
 
+
+
 Act.obj = class extends Act {
     constructor(obj, parent){
-        this.target = obj
+        super(obj, parent)
+        this.$target = obj
         this.$parent = parent;
     }
-    $set(name, value) {
-        super(name);
-        this.target[name] = value;
+    $set(prop, value) {
+        //super(prop);
+        this.$target[prop] = value;
     }
-    $get(name) {
-        super(name);
-        return this.target[name];
+    $get(prop) {
+        //super(prop);
+        return this.$target[prop];
     }
 }
 
@@ -40,11 +58,11 @@ Act.fetch = class extends Act {
     constructor(url) {
         this.$Url = new Url(url);
     }
-    $get(name){
-        return fetch(this.$url+'/'+name).then(res=>res.body());
+    $get(prop){
+        return fetch(this.$url+'/'+prop).then(res=>res.body());
     }
-    $set(name, value){
-        const response = await fetch(this.$url+'/'+name, {
+    async $set(prop, value){
+        const response = await fetch(this.$url+'/'+prop, {
             method: 'POST',
             mode: 'cors',
             cache: 'no-cache',
